dc3d532cb4f20bce86699ef9a847febdb25aa8f7
Martin Wojtczyk <martin.wojtczyk@gmail.com>
2020-05-03 16:56:58 -0700

    listing 3: progress indicator

--- src/main.cc
+++ src/main.cc
@@ -7,6 +7,7 @@ int main() {
   std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
   for (int j = image_height - 1; j >= 0; --j) {
+    std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
     for (int i = 0; i < image_width; ++i) {
       auto r = double(i) / (image_width - 1);
       auto g = double(j) / (image_height - 1);
@@ -19,6 +20,8 @@ int main() {
       std::cout << ir << ' ' << ig << ' ' << ib << '\n';
     }
   }
+  std::cerr << "\nDone.\n";
 
   return 0;
 }
+
