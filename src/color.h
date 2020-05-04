b1cf80d625fa0ae1e0e4c2a721e841ce0fa56cef
Martin Wojtczyk <martin.wojtczyk@gmail.com>
2020-05-03 20:00:37 -0700

    clang-format

--- src/color.h
+++ src/color.h
@@ -8,9 +8,8 @@
 void write_color(std::ostream& out, const Color& pixel_color) {
   // Write the translated [0, 255] value of each color component.
   out << static_cast<int>(255.999 * pixel_color.x()) << ' '
-    << static_cast<int>(255.999 * pixel_color.y()) << ' '
-    << static_cast<int>(255.999 * pixel_color.z()) << '\n';
+      << static_cast<int>(255.999 * pixel_color.y()) << ' '
+      << static_cast<int>(255.999 * pixel_color.z()) << '\n';
 }
 
 #endif
-
--- src/main.cc
+++ src/main.cc
@@ -12,7 +12,8 @@ int main() {
   for (int j = image_height - 1; j >= 0; --j) {
     std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
     for (int i = 0; i < image_width; ++i) {
-      Color pixel_color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0.25);
+      Color pixel_color(double(i) / (image_width - 1),
+                        double(j) / (image_height - 1), 0.25);
       write_color(std::cout, pixel_color);
     }
   }
@@ -20,4 +21,3 @@ int main() {
 
   return 0;
 }
-
