6ff64aefc516a0a86c35d569a0c7ba2fcfe611f7
Martin Wojtczyk <martin.wojtczyk@gmail.com>
2020-05-03 20:19:01 -0700

    listing 9: rendering gradient background

--- src/main.cc
+++ src/main.cc
@@ -1,19 +1,32 @@
 #include "color.h"
 #include "vec3.h"
+#include "ray.h"
 
 #include <iostream>
+
+Color ray_color(const Ray& r) {
+  Vec3 unit_direction = unit_vector(r.direction());
+  auto t = 0.5 * (unit_direction.y() + 1.0);
+  return (1.0 - t) * Color(1.0, 1.0, 1.0) + t * Color(0.5, 0.7, 1.0);
+}
 
 int main() {
   const int image_width = 256;
   const int image_height = 256;
 
   std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-
+  Point3 origin(0.0, 0.0, 0.0);
+  Vec3 horizontal(4.0, 0.0, 0.0);
+  Vec3 vertical(0.0, 2.25, 0.0);
+  Point3 lower_left_corner =
+      origin - horizontal / 2 - vertical / 2 - Vec3(0, 0, 1);
   for (int j = image_height - 1; j >= 0; --j) {
     std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
     for (int i = 0; i < image_width; ++i) {
-      Color pixel_color(double(i) / (image_width - 1),
-                        double(j) / (image_height - 1), 0.25);
+      auto u = double(i) / (image_width - 1);
+      auto v = double(j) / (image_height - 1);
+      Ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+      Color pixel_color = ray_color(r);
       write_color(std::cout, pixel_color);
     }
   }
